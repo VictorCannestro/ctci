9cdde7f1d68f3001569715dd621853c5f99b5676
Victor C Cannestro <vcannestro@gmail.com>
2020-12-26 22:43:31 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='CTCI',
+    author='Victor Cannestro',
+    description='Solutions to problems from the 6th edition of Cracking the Coding Interview',
+    packages=['ctci']
+)
