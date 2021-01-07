6e4e5f8ef4eb185a1e3a9f334eb0f39c5ffd16cf
VictorCannestro <vcannestro@gmail.com>
2021-01-07 12:29:45 -0800

    Done with ch1

--- src/ch1/p9_string_rotation.py
+++ src/ch1/p9_string_rotation.py
@@ -13,17 +13,49 @@ def isRotation(s1,s2):
     Parameters
     ----------
     s1 : str
-        DESCRIPTION.
+        Input string for comparison.
     s2 : str
-        DESCRIPTION.
+        Input string for comparison.
 
     Returns
     -------
     bool.
+        The determination of whether if `s2` is a rotation of `s1`.
+        E.g. "waterbottle" is a rotation of "erbottlewat"
     '''
-    pass
+    if len(s1) != len(s2):
+        return False
+    # Test for 0th order rotation first
+    shifted = s2             
+    for i in range(len(s1)):
+        if s1 == shifted:
+            return True
+        else:
+            # Rotate to the left by 1 char
+            shifted = s2[i:] + s2[:i] 
+    return False
 
+
+def isRotationVer2(s1,s2):
+    '''
+    Assumes the existence of a method `isSubstring` which checks if one 
+    word is a substring of another. Implementation uses only one call to 
+    `isSubstring`. In Python we can just use the `in` keyword.
+    E.g. 
+        "waterbottle" is in "erbottlewat" + "erbottlewat""
+    '''
+    if len(s1) != len(s2):
+        return False
+    elif len(s1) == 0:
+        return False
+    elif s2 in s1+s1:
+        return True
+    else:
+        return False
+    
 
 if __name__ == "__main__":
-    s1 = "ooooooorf"
-    print(isRotation(s1))
+    s1 = "oobleck"
+    s2 = "bleckoo"
+    print(isRotation(s1,s2))
+    print(isRotationVer2(s1,s2))
--- tests/ch1/test_ch1_p9.py
+++ tests/ch1/test_ch1_p9.py
@@ -5,30 +5,55 @@ Created on Wed Jan  6 21:40:56 2021
 @author: Victor Cannestro
 """
 
-from src.ch1.p9_string_rotation import isRotation
+from src.ch1.p9_string_rotation import isRotation, isRotationVer2
 
-class TestBasicCompression(object):
+class TestIsRotation(object):
     ''''''
-    def test_noEffect(self):
-        s1 = "How now" # Compresses to "H1o1w1 1n1o1w1"
-        s2 = ""
-        assert isRotation(s1,s2) == "How now"
+    def test_nomalCase1(self):
+        s1 = "goosebumps" 
+        s2 = "bumpsgoose"
+        assert isRotation(s1,s2) == True
         
-    def test_normalCase(self):
-        s1 = "oooobeeee"
-        s2 = ""
-        assert isRotation(s1,s2) == "How now"
+    def test_normalCase2(self):
+        s1 = "tacocat"
+        s2 = "goose"
+        assert isRotation(s1,s2) == False
         
-    def test_sameLength(self):
-        s1 = "ttccaa" # Compresses to  "t2c2a2"
-        s2 = ""
-        assert isRotation(s1,s2) == "How now"
+    def test_sameWord(self):
+        s1 = "rotation" 
+        assert isRotation(s1,s1) == True # Rotation of order 0
         
-    def test_longer(self):
-        s1 = "hubba" # "h1u1b2a1" is longer
-        s2 = ""
-        assert isRotation(s1,s2) == "How now"
+    def test_ambiguous(self):
+        s1 = "vvvv" 
+        s2 = "vvvv" # Could be a permutation instead of a rotation
+        assert isRotation(s1,s2) == True
         
     def test_empty(self):
         s1 = ""
-        assert isRotation(s1,s1) == ""
+        assert isRotation(s1,s1) == False # By convention
+        
+        
+class TestIsRotationVer2(object):
+    ''''''
+    def test_nomalCase1(self):
+        s1 = "goosebumps" 
+        s2 = "bumpsgoose"
+        assert isRotationVer2(s1,s2) == True
+        
+    def test_normalCase2(self):
+        s1 = "tacocat"
+        s2 = "goose"
+        assert isRotationVer2(s1,s2) == False
+        
+    def test_sameWord(self):
+        s1 = "rotation" 
+        assert isRotationVer2(s1,s1) == True # Rotation of order 0
+        
+    def test_ambiguous(self):
+        s1 = "vvvv" 
+        s2 = "vvvv" # Could be a permutation instead of a rotation
+        assert isRotationVer2(s1,s2) == True
+        
+    def test_empty(self):
+        s1 = ""
+        assert isRotationVer2(s1,s1) == False # By convention
